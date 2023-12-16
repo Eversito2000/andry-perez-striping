f08af815087268da80a70e18a81ca8fbe6cb55a6
Evers <epere439@fiu.edu>
2023-12-15 22:00:12 -0500

    Add icons for all applicable jobs

--- src/App.js
+++ src/App.js
@@ -1,31 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
-import Navbar from './components/Navbar';
-import PastJobs from './components/PastJobs';
-import AboutMe from './components/AboutMe';
-import ApplicableJobs from './components/ApplicableJobs';
-import Grid from '@mui/material/Grid';
-import Container from '@mui/material/Container';
-import Paper from '@mui/material/Paper';
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import PastJobs from "./components/PastJobs";
+import AboutMe from "./components/AboutMe";
+import ApplicableJobs from "./components/ApplicableJobs";
+import Grid from "@mui/material/Grid";
+import Container from "@mui/material/Container";
+import Paper from "@mui/material/Paper";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Container maxWidth="lg" style={{ marginTop: '20px' }}>
+      <Container maxWidth="lg" style={{ marginTop: "20px" }}>
         <Grid container spacing={2}>
-          <Grid item xs={12} style={{ display: 'flex' }}>
+          <Grid item xs={12} style={{ display: "flex" }}>
             <Paper elevation={3} style={{ flexGrow: 1 }}>
               <AboutMe />
             </Paper>
           </Grid>
-          <Grid item xs={12} style={{ display: 'flex' }}>
-            <Paper elevation={3} style={{ flexGrow: 1 }}>
+          <Grid item xs={12} style={{ display: "flex" }}>
+            <Paper
+              elevation={3}
+              style={{ flexGrow: 1, backgroundColor: "transparent", boxShadow: 'none' }}
+            >
               <ApplicableJobs />
             </Paper>
           </Grid>
           <Grid item xs={12}>
-            <Paper style={{ padding: '20px' }}>
+            <Paper style={{ padding: "20px" }}>
               <PastJobs />
             </Paper>
           </Grid>
--- src/components/ApplicableJobs.js
+++ src/components/ApplicableJobs.js
@@ -1,27 +1,37 @@
-import * as React from 'react';
-import { Box, Grid, Card, CardContent, CardMedia, Typography } from '@mui/material';
+import * as React from "react";
+import {
+  Box,
+  Grid,
+  Card,
+  CardContent,
+  CardMedia,
+  Typography,
+} from "@mui/material";
 
 const jobs = [
-    { title: 'Thermoplastic', image: '../img/thermoplastic.png' },
-    { title: 'Striping', image: '../img/unnamed.jpg' },
-    { title: 'Seal Coating', image: '/path/to/image3.jpg' },
-    { title: 'Sign Painting', image: '/path/to/image4.jpg' },
-    { title: 'Pressure Washing', image: '/path/to/image5.jpg' },
-    { title: 'Asphalt', image: '/path/to/image6.jpg' },
-  ];
+  { title: "Thermoplastic", image: require("../img/thermoplastic.png") },
+  { title: "Striping", image: require("../img/parking-lot-sealcoating.png") },
+  { title: "Seal Coating", image: require("../img/sealcoat_watermark.png") },
+  { title: "Sign Painting", image: require("../img/street-sign.png") },
+  { title: "Pressure Washing", image: require("../img/pressure-washing.png") },
+  { title: "Asphalt", image: require("../img/asphalt.webp") },
+];
 
 export default function ApplicableJobs() {
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <Grid container spacing={2}>
+      <Grid container spacing={2} justifyContent="center">
+        {" "}
+        {/* Change this to "center" */}
         {jobs.map((job, index) => (
           <Grid key={index} item xs={12} sm={6} md={4}>
             <Card sx={{ maxWidth: 345 }}>
               <CardMedia
                 component="img"
                 height="140"
-                image={job.image}
+                src={job.image}
                 alt={job.title}
+                sx={{ objectFit: "contain" }}
               />
               <CardContent>
                 <Typography gutterBottom variant="h5" component="div">
