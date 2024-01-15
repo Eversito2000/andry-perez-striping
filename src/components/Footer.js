7117c8bab6b57ed43addff13afcfe157f66b1102
Evers <epere439@fiu.edu>
2024-01-15 12:11:48 -0500

    Update footer to include GitHub reference

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,47 +1,40 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-// import Link from '@mui/material/Link';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-// import LinkedInIcon from '@mui/icons-material/LinkedIn';
-
+import * as React from "react";
+import Box from "@mui/material/Box";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 const theme = createTheme({
-    typography: {
-        fontFamily: 'Rubik',
-    },
+  typography: {
+    fontFamily: "Rubik",
+  },
 });
 const Footer = () => {
-    return (
-        <ThemeProvider theme={theme}>
-            <Box sx={{ flexGrow: 1 }}>
-                <AppBar position="static" sx={{ boxShadow: 'none' }}>
-                    <Toolbar className='Navbar'>
-                        <Box
-                            sx={{
-                                display: 'flex',
-                                flexDirection: 'row',
-                                justifyContent: 'center',
-                                alignItems: 'center',
-                                flexGrow: 1
-                            }}
-                        >
-                            <Typography variant="h7" component="div" sx={{ flexGrow: 1 }}>
-                                Copyright @2024  Created by Evers Perez
-                            </Typography>
-                            {/* <Typography variant="h7" component="div" sx={{ flexGrow: 1 }}>
-                                <Link href="https://www.linkedin.com/in/evers-perez-34516b145/" underline="none" sx={{ color: 'black' }}>
-                                    <LinkedInIcon />
-                                </Link>
-                            </Typography> */}
-                        </Box>
-                    </Toolbar>
-                </AppBar>
+  return (
+    <ThemeProvider theme={theme}>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static" sx={{ boxShadow: "none" }}>
+          <Toolbar className="Navbar">
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "center",
+                alignItems: "center",
+                flexGrow: 1,
+              }}
+            >
+              <Typography variant="h7" component="div" sx={{ flexGrow: 1 }}>
+                Copyright @2024 Created by
+                <a href="https://github.com/Eversito2000" target="_blank"> Evers Perez</a>
+              </Typography>
             </Box>
-        </ThemeProvider>
-    );
+          </Toolbar>
+        </AppBar>
+      </Box>
+    </ThemeProvider>
+  );
 };
 
 export default Footer;
